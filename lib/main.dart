774d45f97ad2a26a9d45e0474d6e99898e7c602b
moses <codeloperstz@gmail.com>
2024-11-01 20:34:23 +0200

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:firebase_remote_config/firebase_remote_config.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:flutter_web_plugins/url_strategy.dart';
 import 'package:flutter/foundation.dart';
 import 'package:package_info_plus/package_info_plus.dart';
@@ -14,7 +13,6 @@ import 'package:semo/splash.dart';
 void main() async {
   if (kIsWeb) usePathUrlStrategy();
   WidgetsFlutterBinding.ensureInitialized();
-  PlatformInAppWebViewController.debugLoggingSettings.enabled = false;
   await initializeFirebase();
   runApp(Semo());
 }

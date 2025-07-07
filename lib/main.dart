8f1079db6b3a8482150e8c6a76a5b59819dc044a
Moses Mbaga <codeloperstz@gmail.com>
2025-07-07 13:53:55 +0300

    Refactor

--- lib/main.dart
+++ lib/main.dart
@@ -1,132 +1,140 @@
-import 'dart:async';
+import "dart:async";
 
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_crashlytics/firebase_crashlytics.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter/foundation.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:semo/firebase_options.dart';
-import 'package:semo/screens/splash.dart';
-import 'package:semo/utils/preferences.dart';
+import "package:firebase_core/firebase_core.dart";
+import "package:firebase_crashlytics/firebase_crashlytics.dart";
+import "package:flutter/material.dart";
+import "package:flutter/foundation.dart";
+import "package:google_fonts/google_fonts.dart";
+import "package:semo/firebase_options.dart";
+import "package:semo/screens/splash.dart";
+import "package:semo/utils/preferences.dart";
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   GoogleFonts.config.allowRuntimeFetching = false;
   await Preferences.init();
   await initializeFirebase();
-  runApp(Semo());
+  runApp(const Semo());
 }
 
-initializeFirebase() async {
+Future<void> initializeFirebase() async {
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
-  FirebaseCrashlytics crashlytics = await FirebaseCrashlytics.instance;
-  runZonedGuarded<Future<void>>(() async {
-    if (!kIsWeb) {
+  if (!kIsWeb) {
+    FirebaseCrashlytics crashlytics = FirebaseCrashlytics.instance;
+    await runZonedGuarded<Future<void>>(() async {
       await crashlytics.setCrashlyticsCollectionEnabled(!kDebugMode);
       FlutterError.onError = crashlytics.recordFlutterFatalError;
-    }
-  }, (error, stack) async {
-    if (!kIsWeb) await crashlytics.recordError(error, stack, fatal: true);
-  });
+    }, (Object error, StackTrace stack) async {
+      await crashlytics.recordError(error, stack, fatal: true);
+    }); 
+  }
 }
 
 class Semo extends StatelessWidget {
   const Semo({super.key});
 
+  static const Color _primary = Color(0xFFAB261D);
+  static const Color _background = Color(0xFF120201);
+  static const Color _surface = Color(0xFF250604);
+  static const Color _onPrimary = Colors.white;
+  static const Color _onSurface = Colors.white54;
+
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Semo',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        useMaterial3: true,
-        brightness: Brightness.dark,
-        primaryColor: Color(0xFFAB261D),
-        scaffoldBackgroundColor: Color(0xFF120201),
-        dialogBackgroundColor: Color(0xFF250604),
-        cardColor: Color(0xFF250604),
-        appBarTheme: AppBarTheme(
-          scrolledUnderElevation: 0,
-          backgroundColor: Color(0xFF120201),
-          titleTextStyle: GoogleFonts.freckleFace(
-            textStyle: TextStyle(
-              fontSize: 24,
-              fontWeight: FontWeight.w900,
-              color: Colors.white,
-            ),
-          ),
-          iconTheme: IconThemeData(color: Colors.white),
-          centerTitle: false,
-        ),
-        textTheme: TextTheme(
-          titleLarge: GoogleFonts.freckleFace(
-            textStyle: TextStyle(
-              fontSize: 38,
-              fontWeight: FontWeight.w900,
-              color: Colors.white,
-            ),
-          ),
-          titleMedium: GoogleFonts.freckleFace(
-            textStyle: TextStyle(
-              fontSize: 32,
-              fontWeight: FontWeight.w900,
-              color: Colors.white,
-            ),
-          ),
-          titleSmall: GoogleFonts.freckleFace(
-            textStyle: TextStyle(
-              fontSize: 24,
-              fontWeight: FontWeight.w900,
-              color: Colors.white,
-            ),
-          ),
-          displayLarge: GoogleFonts.lexend(
-            textStyle: TextStyle(
-              fontSize: 18,
-              color: Colors.white,
-            ),
-          ),
-          displayMedium: GoogleFonts.lexend(
-            textStyle: TextStyle(
-              fontSize: 15,
-              color: Colors.white,
-            ),
-          ),
-          displaySmall: GoogleFonts.lexend(
-            textStyle: TextStyle(
-              fontSize: 14,
-              color: Colors.white,
-            ),
+  Widget build(BuildContext context) => MaterialApp(
+    title: "Semo",
+    debugShowCheckedModeBanner: false,
+    theme: ThemeData(
+      useMaterial3: true,
+      brightness: Brightness.dark,
+      primaryColor: _primary,
+      scaffoldBackgroundColor: _background,
+      cardColor: _surface,
+      appBarTheme: AppBarTheme(
+        scrolledUnderElevation: 0,
+        backgroundColor: _background,
+        titleTextStyle: GoogleFonts.freckleFace(
+          textStyle: const TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.w900,
+            color: _onPrimary,
           ),
         ),
-        progressIndicatorTheme: ProgressIndicatorThemeData(color: Color(0xFFAB261D)),
-        tabBarTheme: TabBarThemeData(
-          indicatorColor: Color(0xFFAB261D),
-          labelColor: Color(0xFFAB261D),
-          dividerColor: Color(0xFF250604),
-          unselectedLabelColor: Colors.white54,
-        ),
-        menuTheme: MenuThemeData(
-          style: MenuStyle(
-            backgroundColor: WidgetStatePropertyAll(Color(0xFF250604)),
-          ),
-        ),
-        bottomSheetTheme: BottomSheetThemeData(
-          showDragHandle: true,
-          backgroundColor: Color(0xFF250604),
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.vertical(
-              top: Radius.circular(25),
-            ),
-          ),
-        ),
-        snackBarTheme: SnackBarThemeData(
-          backgroundColor: Color(0xFF250604),
-          behavior: SnackBarBehavior.floating,
-        )
+        iconTheme: const IconThemeData(color: _onPrimary),
+        centerTitle: false,
       ),
-      home: Splash(),
-    );
-  }
+      textTheme: TextTheme(
+        titleLarge: GoogleFonts.freckleFace(
+          textStyle: const TextStyle(
+            fontSize: 38,
+            fontWeight: FontWeight.w900,
+            color: _onPrimary,
+          ),
+        ),
+        titleMedium: GoogleFonts.freckleFace(
+          textStyle: const TextStyle(
+            fontSize: 32,
+            fontWeight: FontWeight.w900,
+            color: _onPrimary,
+          ),
+        ),
+        titleSmall: GoogleFonts.freckleFace(
+          textStyle: const TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.w900,
+            color: _onPrimary,
+          ),
+        ),
+        displayLarge: GoogleFonts.lexend(
+          textStyle: const TextStyle(
+            fontSize: 18,
+            color: _onPrimary,
+          ),
+        ),
+        displayMedium: GoogleFonts.lexend(
+          textStyle: const TextStyle(
+            fontSize: 15,
+            color: _onPrimary,
+          ),
+        ),
+        displaySmall: GoogleFonts.lexend(
+          textStyle: const TextStyle(
+            fontSize: 14,
+            color: _onPrimary,
+          ),
+        ),
+      ),
+      progressIndicatorTheme: const ProgressIndicatorThemeData(
+        color: _primary,
+      ),
+      dialogTheme: const DialogThemeData(
+        backgroundColor: _surface,
+      ),
+      tabBarTheme: const TabBarThemeData(
+        indicatorColor: _primary,
+        labelColor: _primary,
+        dividerColor: _surface,
+        unselectedLabelColor: _onSurface,
+      ),
+      menuTheme: const MenuThemeData(
+        style: MenuStyle(
+          backgroundColor: WidgetStatePropertyAll<Color>(_surface),
+        ),
+      ),
+      bottomSheetTheme: const BottomSheetThemeData(
+        showDragHandle: true,
+        backgroundColor: _surface,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.vertical(
+            top: Radius.circular(25),
+          ),
+        ),
+      ),
+      snackBarTheme: const SnackBarThemeData(
+        behavior: SnackBarBehavior.floating,
+        backgroundColor: _surface,
+      ),
+    ),
+    home: Splash(),
+  );
 }

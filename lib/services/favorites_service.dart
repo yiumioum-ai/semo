15a9e6dcd2d641896e9a7ba69fdc660f706649a2
Moses Mbaga <codeloperstz@gmail.com>
2025-07-07 23:37:27 +0300

    Refactor

--- lib/services/favorites_service.dart
+++ lib/services/favorites_service.dart
@@ -1,86 +1,117 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import '../utils/db_names.dart';
+import "package:cloud_firestore/cloud_firestore.dart";
+import "package:firebase_auth/firebase_auth.dart";
+import "package:logger/logger.dart";
+import "package:semo/utils/db_names.dart";
 
 class FavoritesService {
-  static final FavoritesService _instance = FavoritesService._internal();
   factory FavoritesService() => _instance;
   FavoritesService._internal();
 
+  static final FavoritesService _instance = FavoritesService._internal();
+
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
   final FirebaseAuth _auth = FirebaseAuth.instance;
+  final Logger _logger = Logger();
 
-  Future<List<int>> getFavoriteMovies() async {
+  DocumentReference<Map<String, dynamic>> _getDocReference() {
     try {
-      final doc = await _firestore
+      return _firestore
           .collection(DB.favorites)
-          .doc(_auth.currentUser!.uid)
-          .get();
+          .doc(_auth.currentUser?.uid);
+    } catch (e, s) {
+      _logger.e("Error getting favorites' document reference", error: e, stackTrace: s);
+    }
+
+    throw Exception("Failed to get favorites' document reference");
+  }
+
+  Future<Map<String, dynamic>> _getFavorites() async {
+    try {
+      final DocumentSnapshot<Map<String, dynamic>> doc = await _getDocReference().get();
 
       if (doc.exists) {
-        final data = doc.data();
-        return ((data?['movies'] ?? []) as List<dynamic>).cast<int>();
+        return doc.data() ?? <String, dynamic>{};
       }
-    } catch (e) {
-      print("Error getting favorite movies: $e");
+    } catch (e, s) {
+      _logger.e("Error getting favorites", error: e, stackTrace: s);
     }
-    return [];
+
+    return <String, dynamic>{};
+  }
+
+  Future<List<int>> getFavoriteMovies() async {
+    Map<String, dynamic> favorites = await _getFavorites();
+
+    try {
+      return ((favorites["movies"] ?? <dynamic>[]) as List<dynamic>).cast<int>();
+    } catch (e, s) {
+      _logger.e("Error getting favorite movies", error: e, stackTrace: s);
+    }
+
+    return <int>[];
   }
 
   Future<List<int>> getFavoriteTvShows() async {
-    try {
-      final doc = await _firestore
-          .collection(DB.favorites)
-          .doc(_auth.currentUser!.uid)
-          .get();
+    Map<String, dynamic> favorites = await _getFavorites();
 
-      if (doc.exists) {
-        final data = doc.data();
-        return ((data?['tv_shows'] ?? []) as List<dynamic>).cast<int>();
-      }
-    } catch (e) {
-      print("Error getting favorite TV shows: $e");
+    try {
+      return ((favorites["tv_shows"] ?? <dynamic>[]) as List<dynamic>).cast<int>();
+    } catch (e, s) {
+      _logger.e("Error getting favorite TV shows", error: e, stackTrace: s);
     }
-    return [];
+
+    return <int>[];
   }
 
   Future<void> addMovieToFavorites(int movieId) async {
-    final favorites = await getFavoriteMovies();
+    List<int> favorites = await getFavoriteMovies();
+
     if (!favorites.contains(movieId)) {
       favorites.add(movieId);
-      await _firestore
-          .collection(DB.favorites)
-          .doc(_auth.currentUser!.uid)
-          .set({'movies': favorites}, SetOptions(merge: true));
+      try {
+        await _getDocReference().set(<String, dynamic>{"movies": favorites}, SetOptions(merge: true));
+      } catch (e, s) {
+        _logger.e("Error adding movie to favorites", error: e, stackTrace: s);
+      }
     }
   }
 
   Future<void> addTvShowToFavorites(int tvShowId) async {
-    final favorites = await getFavoriteTvShows();
+    List<int> favorites = await getFavoriteTvShows();
+
     if (!favorites.contains(tvShowId)) {
       favorites.add(tvShowId);
-      await _firestore
-          .collection(DB.favorites)
-          .doc(_auth.currentUser!.uid)
-          .set({'tv_shows': favorites}, SetOptions(merge: true));
+      try {
+        await _getDocReference().set(<String, dynamic>{"tv_shows": favorites}, SetOptions(merge: true));
+      } catch (e, s) {
+        _logger.e("Error adding TV show to favorites", error: e, stackTrace: s);
+      }
     }
   }
 
   Future<void> removeMovieFromFavorites(int movieId) async {
-    final favorites = await getFavoriteMovies();
-    favorites.remove(movieId);
-    await _firestore
-        .collection(DB.favorites)
-        .doc(_auth.currentUser!.uid)
-        .set({'movies': favorites}, SetOptions(merge: true));
+    List<int> favorites = await getFavoriteMovies();
+
+    if (favorites.contains(movieId)) {
+      favorites.remove(movieId);
+      try {
+        await _getDocReference().set(<String, dynamic>{"movies": favorites}, SetOptions(merge: true));
+      } catch (e, s) {
+        _logger.e("Error removing movie from favorites", error: e, stackTrace: s);
+      }
+    }
   }
 
   Future<void> removeTvShowFromFavorites(int tvShowId) async {
-    final favorites = await getFavoriteTvShows();
-    favorites.remove(tvShowId);
-    await _firestore
-        .collection(DB.favorites)
-        .doc(_auth.currentUser!.uid)
-        .set({'tv_shows': favorites}, SetOptions(merge: true));
+    List<int> favorites = await getFavoriteTvShows();
+
+    if (favorites.contains(tvShowId)) {
+      favorites.remove(tvShowId);
+      try {
+        await _getDocReference().set(<String, dynamic>{"tv_shows": favorites}, SetOptions(merge: true));
+      } catch (e, s) {
+        _logger.e("Error removing TV show from favorites", error: e, stackTrace: s);
+      }
+    }
   }
 }

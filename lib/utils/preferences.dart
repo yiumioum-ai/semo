c74536dac48618e92558ae361387c7316ab89695
Moses Mbaga <codeloperstz@gmail.com>
2025-07-07 15:16:14 +0300

    Refactor

--- lib/utils/preferences.dart
+++ lib/utils/preferences.dart
@@ -1,50 +1,34 @@
-import 'dart:convert';
+import "dart:convert";
 
-import 'package:semo/models/server.dart';
-import 'package:semo/models/subtitle_style.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import "package:semo/models/server.dart";
+import "package:semo/models/subtitle_style.dart";
+import "package:shared_preferences/shared_preferences.dart";
 
 class Preferences {
-  static final Preferences _instance = Preferences._internal();
-
-  factory Preferences() {
-    return _instance;
-  }
+  factory Preferences() => _instance;
 
   Preferences._internal();
-
+  static final Preferences _instance = Preferences._internal();
   static SharedPreferences? _prefs;
 
-  static init() async {
+  static Future<void> init() async {
     _prefs = await SharedPreferences.getInstance();
   }
 
-  setServer(Server server) async {
-    await _prefs?.setString('server', server.name);
-  }
+  Future<bool?> setServer(Server server) async => await _prefs?.setString("server", server.name);
 
-  setSeekDuration(int seekDuration) async {
-    await _prefs?.setInt('seekDuration', seekDuration);
-  }
+  Future<bool?> setSeekDuration(int seekDuration) async => await _prefs?.setInt("seekDuration", seekDuration);
 
-  setSubtitlesStyle(SubtitleStyle subtitlesStyle) async {
-    await _prefs?.setString('subtitleStyle', json.encode(subtitlesStyle.toJson()));
-  }
+  Future<bool?> setSubtitlesStyle(SubtitleStyle subtitlesStyle) async => await _prefs?.setString("subtitleStyle", json.encode(subtitlesStyle.toJson()));
 
-  Future<String> getServer() async {
-    return _prefs?.getString('server') ?? 'Random';
-  }
+  String getServer() => _prefs?.getString("server") ?? "Random";
 
-  Future<int> getSeekDuration() async {
-    return _prefs?.getInt('seekDuration') ?? 15;
-  }
+  int getSeekDuration() => _prefs?.getInt("seekDuration") ?? 15;
 
-  Future<SubtitleStyle> getSubtitlesStyle() async {
-    Map<String, dynamic> data = json.decode(_prefs?.getString('subtitleStyle') ?? '{}');
+  SubtitleStyle getSubtitlesStyle() {
+    Map<String, dynamic> data = json.decode(_prefs?.getString("subtitleStyle") ?? "{}");
     return SubtitleStyle.fromJson(data);
   }
 
-  Future<void> clear() async {
-    await _prefs?.clear();
-  }
+  Future<bool?> clear() async => await _prefs?.clear();
 }

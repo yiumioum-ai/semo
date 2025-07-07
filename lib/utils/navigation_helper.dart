5c7aa4e234a14069607b5ee89db6e42b1cda52ff
Moses Mbaga <codeloperstz@gmail.com>
2025-07-07 16:15:02 +0300

    Refactor

--- lib/utils/navigation_helper.dart
+++ lib/utils/navigation_helper.dart
@@ -1,21 +1,17 @@
-import 'package:flutter/material.dart';
-import 'package:swipeable_page_route/swipeable_page_route.dart';
+import "package:flutter/material.dart";
+import "package:swipeable_page_route/swipeable_page_route.dart";
 
 class NavigationHelper {
-  static Future<dynamic> navigate(
-      BuildContext context,
-      Widget destination, {
-        bool replace = false,
-      }) async {
-    final pageTransition = SwipeablePageRoute(
+  static Future<dynamic> navigate(BuildContext context, Widget destination, {bool replace = false}) {
+    final SwipeablePageRoute<dynamic> pageTransition = SwipeablePageRoute<dynamic>(
       canOnlySwipeFromEdge: true,
       builder: (BuildContext context) => destination,
     );
 
     if (replace) {
-      return await Navigator.pushReplacement(context, pageTransition);
+      return Navigator.pushReplacement(context, pageTransition);
     } else {
-      return await Navigator.push(context, pageTransition);
+      return Navigator.push(context, pageTransition);
     }
   }
 }

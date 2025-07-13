3243b2696c06070b049ec83296a07497c8487aa7
Moses Mbaga <codeloperstz@gmail.com>
2025-07-13 17:04:09 +0300

    Refactor

--- lib/components/spinner.dart
+++ lib/components/spinner.dart
@@ -1,46 +1,10 @@
-import 'dart:async';
+import "dart:async";
 
-import 'package:flutter/material.dart';
+import "package:flutter/material.dart";
 
 class Spinner {
-  BuildContext context;
-
-  Color barrierColor;
-  //Background color of the barrier
-
-  Color? backgroundColor;
-  //Background color of the spinner's view
-
-  Color spinnerColor;
-  //Color of the spinner
-
-  double barrierOpacity;
-  //Opacity of the barrier's background color
-
-  double width;
-  //Width of the spinner's view
-
-  double height;
-  //Height of the spinner's view
-
-  double borderRadius;
-  //Border radius of the spinner's view
-
-  double spinnerMargin;
-  //Margin between the spinner and its view
-
-  Duration transitionDuration;
-  //Enter and exit animations duration
-
-  Duration? duration;
-  //Visibility duration of the loader
-
-  bool? fromNavigatorToShell;
-
-  bool? useRootNavigator;
-
   Spinner(
-      BuildContext this.context, {
+      this.context, {
         this.barrierColor = const Color(0xFF121212),
         this.barrierOpacity = 0.5,
         this.transitionDuration = const Duration(milliseconds: 500),
@@ -49,26 +13,56 @@ class Spinner {
         this.backgroundColor,
         this.borderRadius = 15.0,
         this.spinnerMargin = 16.0,
-        this.spinnerColor = const Color(0xFFAB261D),
+        this.spinnerColor,
         this.duration,
-        this.fromNavigatorToShell = false,
         this.useRootNavigator = true,
       });
 
-  show() async {
+  final BuildContext context;
+
+  final Color barrierColor;
+  //Background color of the barrier
+
+  final Color? backgroundColor;
+  //Background color of the spinner's view
+
+  final Color? spinnerColor;
+  //Color of the spinner
+
+  final double barrierOpacity;
+  //Opacity of the barrier's background color
+
+  final double width;
+  //Width of the spinner's view
+
+  final double height;
+  //Height of the spinner's view
+
+  final double borderRadius;
+  //Border radius of the spinner's view
+
+  final double spinnerMargin;
+  //Margin between the spinner and its view
+
+  final Duration transitionDuration;
+  //Enter and exit animations duration
+
+  final Duration? duration;
+  //Visibility duration of the loader
+
+  final bool useRootNavigator;
+
+  Future<void> show() async {
     await showGeneralDialog(
       context: context,
-      useRootNavigator: useRootNavigator!,
+      useRootNavigator: useRootNavigator,
       barrierDismissible: false,
       barrierColor: barrierColor.withValues(alpha: barrierOpacity),
       transitionDuration: transitionDuration,
-      pageBuilder: (context, animation, secondaryAnimation) {
-        return Container();
-      },
-      transitionBuilder: (context, animation, secondaryAnimation, widget) {
-        return PopScope(
+      pageBuilder: (BuildContext context, Animation<double> animation, Animation<double> secondaryAnimation) => Container(),
+      transitionBuilder: (BuildContext context, Animation<double> animation, Animation<double> secondaryAnimation, Widget widget) => PopScope(
           canPop: false,
-          onPopInvokedWithResult: (didPop, result) {
+          onPopInvokedWithResult: (bool didPop, Object? result) {
             return;
           },
           child: Transform.scale(
@@ -78,20 +72,18 @@ class Spinner {
               child: AlertDialog(
                 backgroundColor: Colors.transparent,
                 elevation: 0,
-                content: Container(
-                  child: Center(
-                    child: Container(
-                      width: width,
-                      height: height,
-                      decoration: BoxDecoration(
-                        color: backgroundColor != null ? backgroundColor : Theme.of(context).dialogBackgroundColor,
-                        borderRadius: BorderRadius.circular(borderRadius),
-                      ),
-                      padding: EdgeInsets.all(spinnerMargin),
-                      child: CircularProgressIndicator(
-                        valueColor: AlwaysStoppedAnimation<Color>(
-                          spinnerColor,
-                        ),
+                content: Center(
+                  child: Container(
+                    width: width,
+                    height: height,
+                    decoration: BoxDecoration(
+                      color: backgroundColor ?? Theme.of(context).dialogTheme.backgroundColor,
+                      borderRadius: BorderRadius.circular(borderRadius),
+                    ),
+                    padding: EdgeInsets.all(spinnerMargin),
+                    child: CircularProgressIndicator(
+                      valueColor: AlwaysStoppedAnimation<Color>(
+                        spinnerColor ?? Theme.of(context).primaryColor,
                       ),
                     ),
                   ),
@@ -99,8 +91,7 @@ class Spinner {
               ),
             ),
           ),
-        );
-      },
+        ),
     ).whenComplete(() {
       if (duration != null) {
         Timer(duration!, () {
@@ -110,11 +101,7 @@ class Spinner {
     });
   }
 
-  dismiss() {
-    if (fromNavigatorToShell!) {
-      Navigator.of(context, rootNavigator: true).pop();
-    } else {
-      Navigator.of(context).pop();
-    }
+  void dismiss() {
+    Navigator.of(context, rootNavigator: useRootNavigator).pop();
   }
 }

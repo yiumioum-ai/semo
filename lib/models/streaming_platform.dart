722b1a7e3b5c8a29504ab30165e7ec2e6a84553b
Moses Mbaga <codeloperstz@gmail.com>
2025-07-13 18:06:52 +0300

    Refactor

--- lib/models/streaming_platform.dart
+++ lib/models/streaming_platform.dart
@@ -1,11 +1,11 @@
 class StreamingPlatform {
-  int id;
-  String logoPath;
-  String name;
-
-  StreamingPlatform({
+  const StreamingPlatform({
     required this.id,
     required this.logoPath,
     required this.name,
   });
+
+  final int id;
+  final String logoPath;
+  final String name;
 }
